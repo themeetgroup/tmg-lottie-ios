5d1ede632a32361ced2529d5913392f2a272f77a
artem-panyushenko <artem.panyushenko@parshipmeet.com>
2025-10-01 10:46:00 +0300

    VPAAS-1908: update package and products names

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
   name: "TMGLottie",
   // Minimum platform versions should be kept in sync with the per-platform targets in Lottie.xcodeproj, lottie-ios.podspec, and lottie-spm's Package.swift
   platforms: [.iOS("13.0"), .macOS("10.15"), .tvOS("13.0"), .custom("visionOS", versionString: "1.0")],
-  products: [.library(name: "Lottie", targets: ["Lottie"])],
+  products: [.library(name: "TMGLottie", targets: ["Lottie"])],
   dependencies: [
     .package(url: "https://github.com/airbnb/swift", .upToNextMajor(from: "1.0.1")),
   ],
