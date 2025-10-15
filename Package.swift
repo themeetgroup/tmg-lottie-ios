41a62773c4245c52f673ecf7155b217a9fe9efd4
Artem Panyushenko <92736212+artem-panyushenko@users.noreply.github.com>
2025-10-15 11:50:51 +0300

    Rename Lottie to TMGLottie in the Package.swift

--- Package.swift
+++ Package.swift
@@ -5,13 +5,13 @@ let package = Package(
   name: "TMGLottie",
   // Minimum platform versions should be kept in sync with the per-platform targets in Lottie.xcodeproj, lottie-ios.podspec, and lottie-spm's Package.swift
   platforms: [.iOS("13.0"), .macOS("10.15"), .tvOS("13.0"), .custom("visionOS", versionString: "1.0")],
-  products: [.library(name: "TMGLottie", targets: ["Lottie"])],
+  products: [.library(name: "TMGLottie", targets: ["TMGLottie"])],
   dependencies: [
     .package(url: "https://github.com/airbnb/swift", .upToNextMajor(from: "1.0.1")),
   ],
   targets: [
     .target(
-      name: "Lottie",
+      name: "TMGLottie",
       path: "Sources",
       exclude: [
         "Private/EmbeddedLibraries/README.md",
